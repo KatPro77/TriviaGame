4af4e7914b4cbf96ae84cf2cd9e5eec1b42f64b7
Katerina Stanley <katerinastanley77@gmail.com>
2018-07-28 08:52:09 -0400

    js updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -59,10 +59,10 @@ var myQuestions = [{
 	answerList: ["Out Of My Way!", "Mind The Gap!", "That's My Seat!", "Cheers, Mate!"],
 	correctAnswer: 2
 }];
-
+    console.log(myQuestions);
 
 //Additional variables
-    var imgArray = ["q1", "q2", "q3", "q4", "q5", "q6", "q7", 'q8', "q9", "q10"];
+    var imgArray = ["q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9", "q10", ];
     var win = rightAnswer;
     var loss = wrongAnswer;
     var currentQuestion;
@@ -79,16 +79,122 @@ var myQuestions = [{
         endTime: "Time is up!",
         endGame: "Check out yoru score",
     }
+    console.log(imgArray);
 
     //Game start and reset functions
-    $(".btn start").click(function() {
+    $("#startBtn").on(click, function() {
         $(this).hide();
         newGame();
     });
 
-    $("#startOverBtn").click(function() {
+    $("#startOverBtn").on(click, function() {
         $(this).hide();
         newGame();
     });
 
+    function newGame() {
+        $("#finalMessage").empty();
+        $("#rightAnswers").empty();
+        $("#wrongAnswers").empty();
+        $("#unanswered").empty();
+        currentQuestion = 0;
+        rightAnswer = 0;
+        wrongAnswer = 0;
+        unanswered = 0;
+        newQuestion();
     
+
+    function newQuestion() {
+        $("#message").empty();
+        $("#wrongAnswers").empty();
+        $("#photo").empty();
+        answered = true;
+    }
+
+    //Quiz part
+    $('#currentQuestion').html('Question #'+(currentQuestion+1)+'/'+myQuestions.length);
+	$('.question').html('<h2>' + myQuestions[currentQuestion].question + '</h2>');
+	for(var i = 0; i < 4; i++){
+		var choices = $('<div>');
+		choices.text(myQuestions[currentQuestion].answerList[i]);
+		choices.attr({'data-index': i });
+		choices.addClass('thisChoice');
+		$('.answerList').append(choices);
+	}
+
+    timing();
+	$('.thisChoice').on('click',function(){
+		userGuess = $(this).data('index');
+		clearInterval(time);
+		answerPage();
+	});
+}
+
+function timing(){
+	seconds = 20;
+	$('#timer').html('<h3>Time Remaining: ' + seconds + '</h3>');
+	answered = true;
+	//sets timer to go down
+	time = setInterval(showCountdown, 1000);
+}
+
+
+function countdown(){
+	seconds--;
+	$('#timeLeft').html('<h3>Time Remaining: ' + seconds + '</h3>');
+	if(seconds < 1){
+		clearInterval(time);
+		answered = false;
+		answerPage();
+	}
+}
+
+
+function answerPage(){
+	$('#currentQuestion').empty();
+	$('.thisChoice').empty(); 
+	$('.question').empty();
+
+	var rightAnswerText = myQuestions[currentQuestion].answerList[myQuestions[currentQuestion].answer];
+	var rightAnswerIndex = myQuestions[currentQuestion].correctAnswer;
+    $('#photo').html('<img src = "assets/images/'+ imgArray[currentQuestion] +'.jpg" width = "400px">');
+    
+	//checks to see correct, incorrect, or unanswered
+	if((userSelect == rightAnswerIndex) && (answered == true)){
+		rightAnswer++;
+		$('#message').html(messages.rightGuess);
+	} else if((userGuess != rightAnswerIndex) && (answered == true)){
+		wrongAnswer++;
+		$('#message').html(messages.wrongGuess);
+		$('#correctedAnswer').html('The correct answer was: ' + rightAnswerText);
+	} else{
+		unanswered++;
+		$('#message').html(messages.endTime);
+		$('#correctedAnswer').html('The correct answer was: ' + rightAnswerText);
+		answered = true;
+	}
+	
+	if(currentQuestion == (myQuestions.length-1)){
+		setTimeout(scoreboard, 5000)
+	} else{
+		currentQuestion++;
+		setTimeout(newQuestion, 5000);
+	}	
+}
+
+function scoreboard(){
+	$('#timer').empty();
+	$('#message').empty();
+	$('#correctedAnswer').empty();
+	$('#photo').empty();
+
+	$('#finalMessage').html(messages.finished);
+	$('#correctAnswers').html("Correct Answers: " + rightAnswer);
+	$('#incorrectAnswers').html("Incorrect Answers: " + wrongAnswer);
+	$('#unanswered').html("Unanswered: " + unanswered);
+	$('#startOverBtn').addClass('reset');
+	$('#startOverBtn').show();
+	$('#startOverBtn').html('Start Over?');
+}
+
+
