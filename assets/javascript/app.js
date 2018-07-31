6156a197306a09d91e2f7a18ca48d5d4ffe97165
Katie Stanley <KStanley@optimaengineering.com>
2018-07-31 08:12:11 -0400

    updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,4 @@
+$(document).ready(function(){
 // Variables for trivia questions with boolean values (answers)
 
 var myQuestions = [{
@@ -7,130 +8,132 @@ var myQuestions = [{
 },
 
 {
-    question: "What area of London was used to make J. Roberts and H.Grant 1999 rom-com?",
+	question: "What area of London was used to make J. Roberts and H.Grant 1999 rom-com?",
 	answerList: ["Camden Town", "Notting Hill", "Hyde Park", "Primrose Hill"],
 	correctAnswer: 1
 },
 
 {
-    question: "Who lives at 10 Downing Street?",
+	question: "Who lives at 10 Downing Street?",
 	answerList: ["The Prime Minister", "Spice Girls", "The Queen", "Sherlock Holmes"],
 	correctAnswer: 0
 },
 
 {
-    question: "Where in London is the home to a major annual tennis tournamnet?",
+	question: "Where in London is the home to a major annual tennis tournamnet?",
 	answerList: ["Regent's Park", "Wimbledon", "Hampstead Heath", "Covent Garden"],
 	correctAnswer: 1
 },
 
 {
-    question: "Which London venue hosted tribute concerts for Live Aid, Freddy Mercury and Princess Diana?",
+	question: "Which London venue hosted tribute concerts for Live Aid, Freddy Mercury and Princess Diana?",
 	answerList: ["Westminster Abbey", "Royal Albert Hall", "Hammersmith Apollo", "Wembley Arena"],
 	correctAnswer: 3
 },
 
 {
-    question: "Which famous detective lived at 221B Baker Street?",
+	question: "Which famous detective lived at 221B Baker Street?",
 	answerList: ["Jack the Ripper", "Sherlock Holmes", "Columbo", "Kojak"],
 	correctAnswer: 1
 },
 
 {
-    question: "Which tourist spot is known for shopping and for its large electronic billboards?",
+	question: "Which tourist spot is known for shopping and for its large electronic billboards?",
 	answerList: ["Piccadilly Circus", "Camden Town", "West End", "Notting Hill"],
 	correctAnswer: 0
 },
 
 {
-    question: "What are HARRODS and SELFRIDGES?",
+	question: "What are HARRODS and SELFRIDGES?",
 	answerList: ["Department Stores", "Traditional British Candy", "UK's version of Bonnie & Clyde", "Names of The Queens favorite dogs"],
 	correctAnswer: 1
 },
 
 {
-    question: "Which childrens book character has its own statue in Kensington Gardes?",
+	question: "Which childrens book character has its own statue in Kensington Gardes?",
 	answerList: ["Harry Potter", "Mary Poppins", "Teletubbies", "Peter Pan"],
 	correctAnswer: 3
 },
 
 {
-    question: "What is the famous phrase used in London Underground (Subway)?",
+	question: "What is the famous phrase used in London Underground (Subway)?",
 	answerList: ["Out Of My Way!", "Mind The Gap!", "That's My Seat!", "Cheers, Mate!"],
 	correctAnswer: 2
 }];
-    console.log(myQuestions);
+console.log(myQuestions);
 
 //Additional variables
-    var imgArray = ["q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9", "q10", ];
-    var win = rightAnswer;
-    var loss = wrongAnswer;
-    var currentQuestion;
-    var rightAnswer;
-    var wrongAnswer;
-    var userGuess;
-    var answered;
-    var unanswered;
-    var time;
-    var seconds;
-    var messages = {
-        correctGuess: "CORRECT!",
-        wrongGuess: "SORRY, WRONG ANSWER!",
-        endTime: "Time is up!",
-        endGame: "Check out yoru score",
-    }
-    console.log(imgArray);
+var imgArray = ["q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9", "q10",];
+var win = rightAnswer;
+var loss = wrongAnswer;
+var currentQuestion;
+var rightAnswer;
+var wrongAnswer;
+var userGuess;
+var answered;
+var unanswered;
+var time;
+var seconds;
+var messages = {
+	correctGuess: "CORRECT!",
+	wrongGuess: "SORRY, WRONG ANSWER!",
+	endTime: "Time is up!",
+	endGame: "Check out yoru score",
+}
+console.log(imgArray);
 
-    //Game start and reset functions
-    $("#startBtn").on(click, function() {
-        $(this).hide();
-        newGame();
-    });
+//Game start and reset functions
+$("#startBtn").on("click", function () {
+	console.log("check in click")
+	$(this).hide();
+	newGame();
 
-    $("#startOverBtn").on(click, function() {
-        $(this).hide();
-        newGame();
-    });
+});
 
-    function newGame() {
-        $("#finalMessage").empty();
-        $("#rightAnswers").empty();
-        $("#wrongAnswers").empty();
-        $("#unanswered").empty();
-        currentQuestion = 0;
-        rightAnswer = 0;
-        wrongAnswer = 0;
-        unanswered = 0;
-        newQuestion();
-    
+$("#startOverBtn").on("click", function () {
+	$(this).hide();
+	newGame();
+});
 
-    function newQuestion() {
-        $("#message").empty();
-        $("#wrongAnswers").empty();
-        $("#photo").empty();
-        answered = true;
-    }
+function newGame() {
+	$("#finalMessage").empty();
+	$("#rightAnswers").empty();
+	$("#wrongAnswers").empty();
+	$("#unanswered").empty();
+	currentQuestion = 0;
+	rightAnswer = 0;
+	wrongAnswer = 0;
+	unanswered = 0;
+	newQuestion();
 
-    //Quiz part
-    $('#currentQuestion').html('Question #'+(currentQuestion+1)+'/'+myQuestions.length);
+
+	function newQuestion() {
+		$("#message").empty();
+		$("#wrongAnswers").empty();
+		$("#photo").empty();
+		answered = true;
+	}
+
+	//Quiz part
+	$('#currentQuestion').html('Question #' + (currentQuestion + 1) + '/' + myQuestions.length);
 	$('.question').html('<h2>' + myQuestions[currentQuestion].question + '</h2>');
-	for(var i = 0; i < 4; i++){
+	for (var i = 0; i < 4; i++) {
 		var choices = $('<div>');
 		choices.text(myQuestions[currentQuestion].answerList[i]);
-		choices.attr({'data-index': i });
+		choices.attr({ 'data-index': i });
 		choices.addClass('thisChoice');
 		$('.answerList').append(choices);
 	}
 
-    timing();
-	$('.thisChoice').on('click',function(){
+	timing();
+	$('.thisChoice').on('click', function () {
 		userGuess = $(this).data('index');
 		clearInterval(time);
 		answerPage();
 	});
 }
 
-function timing(){
+function timing() {
 	seconds = 20;
 	$('#timer').html('<h3>Time Remaining: ' + seconds + '</h3>');
 	answered = true;
@@ -139,10 +142,10 @@ function timing(){
 }
 
 
-function countdown(){
+function countdown() {
 	seconds--;
 	$('#timeLeft').html('<h3>Time Remaining: ' + seconds + '</h3>');
-	if(seconds < 1){
+	if (seconds < 1) {
 		clearInterval(time);
 		answered = false;
 		answerPage();
@@ -150,39 +153,39 @@ function countdown(){
 }
 
 
-function answerPage(){
+function answerPage() {
 	$('#currentQuestion').empty();
-	$('.thisChoice').empty(); 
+	$('.thisChoice').empty();
 	$('.question').empty();
 
 	var rightAnswerText = myQuestions[currentQuestion].answerList[myQuestions[currentQuestion].answer];
 	var rightAnswerIndex = myQuestions[currentQuestion].correctAnswer;
-    $('#photo').html('<img src = "assets/images/'+ imgArray[currentQuestion] +'.jpg" width = "400px">');
-    
+	$('#photo').html('<img src = "assets/images/' + imgArray[currentQuestion] + '.jpg" width = "400px">');
+
 	//checks to see correct, incorrect, or unanswered
-	if((userSelect == rightAnswerIndex) && (answered == true)){
+	if ((userSelect == rightAnswerIndex) && (answered == true)) {
 		rightAnswer++;
 		$('#message').html(messages.rightGuess);
-	} else if((userGuess != rightAnswerIndex) && (answered == true)){
+	} else if ((userGuess != rightAnswerIndex) && (answered == true)) {
 		wrongAnswer++;
 		$('#message').html(messages.wrongGuess);
 		$('#correctedAnswer').html('The correct answer was: ' + rightAnswerText);
-	} else{
+	} else {
 		unanswered++;
 		$('#message').html(messages.endTime);
 		$('#correctedAnswer').html('The correct answer was: ' + rightAnswerText);
 		answered = true;
 	}
-	
-	if(currentQuestion == (myQuestions.length-1)){
+
+	if (currentQuestion == (myQuestions.length - 1)) {
 		setTimeout(scoreboard, 5000)
-	} else{
+	} else {
 		currentQuestion++;
 		setTimeout(newQuestion, 5000);
-	}	
+	}
 }
 
-function scoreboard(){
+function scoreboard() {
 	$('#timer').empty();
 	$('#message').empty();
 	$('#correctedAnswer').empty();
@@ -197,4 +200,5 @@ function scoreboard(){
 	$('#startOverBtn').html('Start Over?');
 }
 
+});
 
